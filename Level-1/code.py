d047dac79b0453d97074dc039516a80138aa462b
Kristina Jovanovska <kristinajovanovska993@gmail.com>
2023-03-31 21:55:03 +0200

    level 1

--- Level-1/code.py
+++ Level-1/code.py
@@ -1,7 +1,7 @@
 '''
 ////////////////////////////////////////////////////////////
 ///                                                      ///
-///   0. tests.py is passing but the code is vulnerable  /// 
+///   0. tests.py is passing but the code is vulnerable  ///
 ///   1. Review the code. Can you spot the bug?          ///
 ///   2. Fix the code but ensure that tests.py passes    ///
 ///   3. Run hack.py and if passing then CONGRATS!       ///
@@ -16,18 +16,23 @@ from collections import namedtuple
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+
 def validorder(order: Order):
     net = 0
-    
-    for item in order.items:
-        if item.type == 'payment':
-            net += item.amount
-        elif item.type == 'product':
-            net -= item.amount * item.quantity
-        else:
-            return("Invalid item type: %s" % item.type)
-    
-    if net != 0:
-        return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
-    else:
-        return("Order ID: %s - Full payment received!" % order.id)
+
+    for item in range(len(order.items)):
+        if order.items[item].type == 'payment' and 500 < order.items[item].amount < 100000:
+            net += order.items[item].amount
+            print('pr', net)
+        elif order.items[item].type == 'product':
+            if order.items[item].quantity < 500 and 1 <= order.items[item].amount < 100000:
+                net -= order.items[item].amount * order.items[item].quantity
+            if -100000 > float(net) > 100000:
+                return "Total amount exceeded"
+        elif order.items[item].type not in ('product', 'payment'):
+            return ("Invalid item type: %s" % order.items[item].type)
+
+        if net != 0:
+            return ("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
+        elif net > 0:
+            return ("Order ID: %s - Full payment received!" % order.id)
